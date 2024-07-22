4ab88e867cbf26748f6ae86a16b9432fbaeb4aff
Weksslei Rodrigues <weksslei@easyteam.rocks>
2024-07-22 11:32:16 -0300

    insered player and removed comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,28 @@
-import { Component, ElementRef ,Renderer2 } from '@angular/core';
-import { HostListener } from '@angular/core';
+import { Component, ElementRef, Renderer2, OnInit } from '@angular/core';
+import { RadioService } from './radio.service'; // Certifique-se de importar o serviço de rádio
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-
-export class AppComponent {
-
-  constructor(private renderer: Renderer2, private element: ElementRef) {}
+export class AppComponent implements OnInit {
   selectedStation: any;
   categories = [
     'MPB', 'Sertanejo Brasil', 'Rádio FM', 'Radio Sertanejo Só Modão',
     'Bossa Nova Brasil', 'Gaúcha', 'Sertanejo Brasil', 'Rádio Jovem Pan', 'Novo Tempo'
   ];
-  stations = [
-    { name: 'CNR-1 中国之声', country: 'China, news' },
-    { name: 'Sertanejo Brasil', country: 'Brasil, RJ' },
-    { name: 'Rádio FM', country: 'Brasil, FM' },
-    { name: 'Radio Sertanejo Só Modão', country: 'moda, moda de viola, modao, sertanejo' },
-    { name: 'France Inter', country: 'France' }
-  ];
+  stations: any[] = [];
+
+  constructor(private renderer: Renderer2, private element: ElementRef, private radioService: RadioService) {}
+
+  ngOnInit() {
+    this.radioService.getStations().subscribe(data => {
+      this.stations = data;
+    });
+  }
 
   isMenuVisible = false;
-
-
 
   onStationSelected(station: any) {
     this.selectedStation = station;
@@ -40,20 +37,20 @@ export class AppComponent {
     if (sidebar && mainContent) {
       if (!this.isMenuVisible) {
         this.renderer.removeClass(sidebar, 'col-md-3');
-        this.renderer.addClass(sidebar, 'col-1')
+        this.renderer.addClass(sidebar, 'col-1');
         this.renderer.removeClass(mainContent, 'col-md-9');
         this.renderer.addClass(mainContent, 'col-11');
         this.renderer.removeClass(sidebar, 'mobile-hide');
       } else {
-        this.renderer.removeClass(sidebar, 'col-1')
+        this.renderer.removeClass(sidebar, 'col-1');
         this.renderer.addClass(sidebar, 'col-md-3');
         this.renderer.removeClass(mainContent, 'col-11');
         this.renderer.addClass(mainContent, 'col-md-9');
         this.renderer.addClass(sidebar, 'mobile-hide');
       }
-
     } else {
       console.log('Não achou os elementos');
     }
   }
 }
+``
